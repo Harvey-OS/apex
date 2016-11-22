32bb1dcfad20fc9ac55889ac57175be1f5c4f89a
Álvaro Jurado <elbingmiss@gmail.com>
2016-11-23 00:43:43 +0100

    Adding gettext and others needed for flex
    This is a small step for avoiding libiberty in future.
    
    - expr.c was just forgotten wrongly in previous PR
    
    Signed-off-by: Álvaro Jurado <elbingmiss@gmail.com>

---
+++ lib/ap/bsd/dirname.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2005-2014 Rich Felker, et al.
+ * Copyright (c) 2015-2016 Álvaro Jurado et al.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE.mit file.
+ */
+
+#include <string.h>
+#include <libgen.h>
+
+char *dirname(char *s)
+{
+	size_t i;
+	if (!s || !*s) return ".";
+	i = strlen(s)-1;
+	for (; s[i]=='/'; i--) if (!i) return "/";
+	for (; s[i]!='/'; i--) if (!i) return ".";
+	for (; s[i]=='/'; i--) if (!i) return "/";
+	s[i+1] = 0;
+	return s;
+}
