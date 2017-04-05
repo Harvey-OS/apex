80391dfe5cc2bdd31a5ad10958894d98941a162c
Álvaro Jurado <elbingmiss@gmail.com>
2017-04-05 03:32:38 +0200

    Fixing fwrite from musl
    https://git.musl-libc.org/cgit/musl/commit/src/stdio/fwrite.c?id=500c6886c654fd45e4926990fee2c61d816be197
    https://git.musl-libc.org/cgit/musl/commit/src/stdio/fwrite.c?id=5a6e8d098abfef67028e6c3edbac39e0bbaf8bd8
    
    Signed-off-by: Álvaro Jurado <elbingmiss@gmail.com>

--- lib/ap/stdio/fwrite.c
+++ lib/ap/stdio/fwrite.c
@@ -21,8 +21,8 @@ size_t __fwritex(const unsigned char *restrict s, size_t l, FILE *restrict f)
 		/* Match /^(.*\n|)/ */
 		for (i=l; i && s[i-1] != '\n'; i--);
 		if (i) {
-			if (f->write(f, s, i) < i)
-				return i;
+			size_t n = f->write(f, s, i);
+			if (n < i) return n;
 			s += i;
 			l -= i;
 		}
@@ -36,6 +36,7 @@ size_t __fwritex(const unsigned char *restrict s, size_t l, FILE *restrict f)
 size_t fwrite(const void *restrict src, size_t size, size_t nmemb, FILE *restrict f)
 {
 	size_t k, l = size*nmemb;
+	if (!size) nmemb = 0;
 	FLOCK(f);
 	k = __fwritex(src, l, f);
 	FUNLOCK(f);
