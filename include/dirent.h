df4feaf77319d2832ea97564fe828ab1ff5fd393
Álvaro Jurado <elbingmiss@gmail.com>
2017-05-11 03:04:12 +0200

    New constants for dirent.h
    Signed-off-by: Álvaro Jurado <elbingmiss@gmail.com>

--- include/dirent.h
+++ include/dirent.h
@@ -51,6 +51,21 @@ int            readdir_r(DIR *__restrict, struct dirent *__restrict, struct dire
 void		rewinddir(DIR *);
 int		closedir(DIR *);
 
+#if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
+#define DT_UNKNOWN 0
+#define DT_FIFO 1
+#define DT_CHR 2
+#define DT_DIR 4
+#define DT_BLK 6
+#define DT_REG 8
+#define DT_LNK 10
+#define DT_SOCK 12
+#define DT_WHT 14
+#define IFTODT(x) ((x)>>12 & 017)
+#define DTTOIF(x) ((x)<<12)
+int getdents(int, struct dirent *, size_t);
+#endif
+
 #ifdef __cplusplus
 }
 #endif
