985c7f5260d768e02b31b28adfd3eebc23dfdce1
Álvaro Jurado <elbingmiss@gmail.com>
2017-11-30 00:42:10 +0100

    Removing segdettach and last old buf dependencies
    Signed-off-by: Álvaro Jurado <elbingmiss@gmail.com>

--- lib/ap/plan9/_read.c
+++ lib/ap/plan9/_read.c
@@ -18,8 +18,7 @@
 ssize_t
 __read(int d, void *buf, size_t nbytes)
 {
-	int n, noblock, isbuf;
-	Fdinfo *f;
+	int n;
 
 	if(d<0 || d>=OPEN_MAX || !(_fdinfo[d].flags&FD_ISOPEN)){
 		errno = EBADF;
@@ -32,25 +31,11 @@ __read(int d, void *buf, size_t nbytes)
 		return -1;
 	}
 
-	f = &_fdinfo[d];
-	noblock = f->oflags&O_NONBLOCK;
-	isbuf = f->flags&(FD_BUFFERED|FD_BUFFEREDX);
-	if(noblock || isbuf){
-		if(f->flags&FD_BUFFEREDX) {
-			errno = EIO;
-			return -1;
-		}
-		if(!isbuf) {
-			if(_startbuf(d) != 0) {
-				errno = EIO;
-				return -1;
-			}
-		}
-		n = _readbuf(d, buf, nbytes, noblock);
-	}else{
-		n = __sys_read(d, buf, nbytes);
-		if(n < 0)
-			_syserrno();
-	}
+	if(_fdinfo[d].oflags&O_APPEND)
+		seek(d, 0, 2);
+	n = __sys_read(d, buf, nbytes);
+	if(n < 0)
+		_syserrno();
+
 	return n;
 }
