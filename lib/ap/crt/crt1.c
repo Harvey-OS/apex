141ced2cacf39a9f51c357a8d516bfe218352c2e
Álvaro Jurado <elbingmiss@gmail.com>
2018-11-26 00:59:03 +0100

    Added proper license headers
    Signed-off-by: Álvaro Jurado <elbingmiss@gmail.com>

--- lib/ap/crt/crt1.c
+++ lib/ap/crt/crt1.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2005-2014 Rich Felker, et al.
+ * Copyright (c) 2015-2018 HarveyOS et al.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE.mit file.
+ */
 
 #define START "_start"
 
